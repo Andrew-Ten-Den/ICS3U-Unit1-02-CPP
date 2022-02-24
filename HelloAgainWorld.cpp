9ce69f73e199d2b305f516e8adbc33bd24a5d70a
Andrew-Ten-Den <andrew.ten-den@mths.ca>
2022-02-24 19:52:00 +0000

    modified hello again world to fix linter

--- HelloAgainWorld.cpp
+++ HelloAgainWorld.cpp
@@ -1,5 +1,7 @@
+// Copyright (c) 2022 Andrew-Ten-Den All rights reserved
+
 #include <iostream>
 
 int main() {
-	std::cout << "Hello Again World" << std::endl;
+    std::cout << "Hello Again World" << std::endl;
 }
